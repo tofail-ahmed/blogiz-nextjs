5f278a674839dae2544bff5f94fe13672e966ac6
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-29 10:30:37 +0600

    Show Off All The Fantastic Blogs Using SSR;

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,33 @@
+import BlogCard from "@/components/ui/BlogCard";
+import { BlogPost } from "@/types";
+import React from "react";
+
+const BlogsPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs",{
+      cache:"no-store"
+  });
+  const blogs = await res.json();
+  return (
+    <div className="w-[90%] mx-auto">
+      <h1 className="text-4xl text-center my-5">
+        Latest Blogs from <span className="text-accent">Blogiz</span>
+      </h1>
+      <h1 className="mx-auto  text-gray-600 text-center">
+        {" "}
+        <i>
+          Dive into the fascinating world of quantum computing, <br /> where
+          unlocking unprecedented computational power.
+        </i>
+      </h1>
+      <div className="grid grid-cols-3 gap-4 my-5">
+            {
+                  blogs.map((blog:BlogPost)=>
+                  <BlogCard key={blog.id} blog={blog}/>
+                  )
+            }
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
 
 const HomePage = async () => {
-  const res=await fetch("http://localhost:5000/blogs");
+  const res=await fetch("http://localhost:5000/blogs",{
+    next:{
+      revalidate:30,
+    }
+  });
   const blogs=await res.json();
   return (
     <>
