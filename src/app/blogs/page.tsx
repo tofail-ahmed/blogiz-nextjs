32244747eebc4e7ba0d6637513a0f585f881c076
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-30 00:33:20 +0600

    Displaying Blog Data Using Redux

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,12 +1,16 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { BlogPost } from "@/types";
 import React from "react";
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs",{
-      cache:"no-store",
-  });
-  const blogs = await res.json();
+const BlogsPage =  () => {
+  // const res = await fetch("http://localhost:5000/blogs",{
+  //     cache:"no-store",
+  // });
+  // const blogs = await res.json();
+  const {data:blogs,isLoading,isFetching,isError,error}=useGetBlogsQuery("");
+  console.log(blogs)
   return (
     <div className="w-[90%] mx-auto">
       <h1 className="text-4xl text-center my-5">
@@ -21,7 +25,7 @@ const BlogsPage = async () => {
       </h1>
       <div className="grid grid-cols-3 gap-4 my-5">
             {
-                  blogs.map((blog:BlogPost)=>
+                  blogs?.map((blog:BlogPost)=>
                   <BlogCard key={blog.id} blog={blog}/>
                   )
             }
